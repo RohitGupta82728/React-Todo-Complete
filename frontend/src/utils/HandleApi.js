a9fbe410b69847e2ee4830938fb69fcb070885a9
RohitGupta82728 <rohitgupta82728@gmail.com>
2023-04-24 19:51:21 +0530

    test

---
+++ frontend/src/utils/HandleApi.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:5000";
+
+const getAllTodo = (setTodo) =>{
+    axios
+    .get(baseUrl)
+    .then(({data})=>{
+        console.log("data -->",data)
+        setTodo(data);
+    });
+    
+}
+
+const addTodo = (text,setText,setTodo) =>{
+    axios
+    .post(`${baseUrl}/save`,{text})
+    .then((data)=>{
+        console.log(data);
+        setText("");
+        getAllTodo(setTodo);
+    })
+    .catch((error)=>console.log(error));
+}
+
+const updateTodo = (todoId,text,setTodo,setText,setIsUpdating) =>{
+    axios
+    .post(`${baseUrl}/update`,{_id:todoId, text})
+    .then((data)=>{
+        //console.log(data);
+        setText("");
+        setIsUpdating(false);
+        getAllTodo(setTodo);
+    }).catch((error)=>console.log(error));
+}
+
+
+const deleteTodo = (_id,setTodo) =>{
+    axios
+    .post(`${baseUrl}/delete`,{_id})
+    .then((data)=>{
+        getAllTodo(setTodo);
+    }).catch((error)=>console.log(error));
+}
+
+export {getAllTodo,addTodo,updateTodo,deleteTodo}
